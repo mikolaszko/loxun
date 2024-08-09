0cafd0cd1e052d196acfe4e326693b31722596e4
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-08-09 19:58:15 +0200

    recognizing longer lexemes

---
+++ src/com/mikolaszko/loxun/TokenType.java
@@ -0,0 +1,22 @@
+package loxun;
+
+
+enum TokenType {
+  // Si tokens
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // ONE or two character tokens,
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals
+  IDENTIFIER, STRING, NUMBER,
+
+  // KEYWORDS,
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+
+  EOF
+}
