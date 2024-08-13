fcbfd3b269bd863d6c69b4474cb0d47f6297c4ed
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-08-13 11:45:07 +0200

    ast printer implementation

--- src/com/mikolaszko/loxun/Expr.java
+++ src/com/mikolaszko/loxun/Expr.java
@@ -1,14 +1,23 @@
 package loxun;
 
-import java.util.List;
-
 abstract public class Expr {
+    interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+ }
  public static class Binary extends Expr {
     public Binary(Expr left, Token operator, Expr right) {
         this.left = left;
         this.operator = operator;
         this.right = right;
      }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+        return visitor.visitBinaryExpr(this);
+    }
 
     final Expr left;
     final Token operator;
@@ -19,12 +28,22 @@ abstract public class Expr {
         this.expression = expression;
      }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+        return visitor.visitGroupingExpr(this);
+    }
+
     final Expr expression;
  }
  public static class Literal extends Expr {
     public Literal(Object value) {
         this.value = value;
      }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+        return visitor.visitLiteralExpr(this);
+    }
 
     final Object value;
  }
@@ -34,7 +53,14 @@ abstract public class Expr {
         this.right = right;
      }
 
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+        return visitor.visitUnaryExpr(this);
+    }
+
     final Token operator;
     final Expr right;
  }
+
+ abstract <R> R accept(Visitor<R> visitor);
 }
